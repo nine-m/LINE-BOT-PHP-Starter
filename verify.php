c9e2b5421df112773654d0db48d07c29e62c6ab3
nine-m <chedsada@gmail.com>
2017-09-12 21:10:18 +0700

    add fixie proxy support

--- verify.php
+++ verify.php
@@ -1,4 +1,7 @@
 <?php
+$proxy = 'velodrome.usefixie.com:80';
+$proxyauth = 'fixie:NnwPBqm5tR6Jtx5';
+
 $access_token = 'QDd6Ba0wLQlflnU/kxWlc3rGTLBX/ioj+2T/SfAp8oxT/1p2sEZoOLhXJAL21PxO2hw+TCQ+Xlqhr/aGozTEsZRf7yDIwV+j/hKYlIxpBDUmnOE3IQIXzbY8BY12i66iaKVRzuJ6Z3WYnxIA27xSdwdB04t89/1O/w1cDnyilFU=';
 
 $url = 'https://api.line.me/v1/oauth/verify';
@@ -9,6 +12,8 @@ $ch = curl_init($url);
 curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
 curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
 curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
+curl_setopt($ch, CURLOPT_PROXY, $proxy); //proxy
+curl_setopt($ch, CURLOPT_PROXYUSERPWD, $proxyauth); //proxy
 $result = curl_exec($ch);
 curl_close($ch);
 
