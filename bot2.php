4fadc17270e7734cb76a7f852ccd8147c3df153c
nine-m <chedsada@gmail.com>
2017-09-13 10:28:36 +0700

    use line bot sdk

--- bot2.php
+++ bot2.php
@@ -3,10 +3,20 @@ include ('line-bot.php');
 $channelSecret = '8c8824d765fe14b40b05dc83294764a5';
 $access_token  = 'QDd6Ba0wLQlflnU/kxWlc3rGTLBX/ioj+2T/SfAp8oxT/1p2sEZoOLhXJAL21PxO2hw+TCQ+Xlqhr/aGozTEsZRf7yDIwV+j/hKYlIxpBDUmnOE3IQIXzbY8BY12i66iaKVRzuJ6Z3WYnxIA27xSdwdB04t89/1O/w1cDnyilFU=';
 $bot = new BOT_API($channelSecret, $access_token);
+
+//mlab config
+$mlab_api_key = 'wGv_MG_7RHOetlGwfsSENc5p-A2J9LcC';
+$mlab_url = 'https://api.mlab.com/api/1/databases/nine-m/collections/linebot?apiKey='.$mlab_api_key.'';
 	
 if (!empty($bot->isEvents)) {
-		
-    $bot->replyMessageNew($bot->replyToken, json_encode($bot->message));
+
+    if($bot->isText){
+        $bot->replyMessageNew($bot->replyToken, json_encode($bot->text));
+    } else {
+        $bot->replyMessageNew($bot->replyToken, json_encode($bot->message));
+    }
+
+	
     if ($bot->isSuccess()) {
         echo 'Succeeded!';
         exit();
