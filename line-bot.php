b58389c8e40cdc06c9d23016d6f1a2fcb5a7d7bf
nine-m <chedsada@gmail.com>
2017-09-13 14:45:05 +0700

    Add customize class LineAPI

--- line-bot.php
+++ line-bot.php
@@ -104,7 +104,7 @@ class BOT_API extends LINEBot {
     }
 
     public function replyLocation ($title,$address,$lat,$long) {
-        $messageBuilder = new \LINE\LINEBot\MessageBuilder\LocationMessageBuilder($title ,$address, $lat, $long);
+        $messageBuilder = new \LINE\LINEBot\MessageBuilder\LocationMessageBuilder("Eiffel Tower", "Champ de Mars, 5 Avenue Anatole France, 75007 Paris, France", 48.858328, 2.294750);
         $this->response = $this->httpClient->post($this->endpointBase . '/v2/bot/message/push', [
             'to' => $this->userid,
             'messages'   => $messageBuilder->buildMessage(),
